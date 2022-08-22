874740ef71db225ed4dbae4b5c3f772eadd79d88
Andrew Wang <Andrewruujin@gmail.com>
2022-08-22 17:34:24 -0400

    commit tree d96f01a13c27a515f62cd92bde34705e528e55a7

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solutions in this file
+
+const writeCard = ["Guadalupe", "Ollie", "Aki"];
+const newCard = [];
+let countDownNum;
+function writeCards (){
+
+    for (let i = 0; i < writeCard.length; i++){
+        newCard.push(`Thank you, ${writeCard[i]}, for the wonderful surprise gift!`)
+        debugger;
+    }
+
+    return newCard;
+
+}
+
+function countDown(x){
+    let countDownNum =x;
+    while(countDownNum>=0){
+        console.log(countDownNum);
+        --countDownNum;
+    }
+     
+}
+
+countDown(4);
+countDown(10);
